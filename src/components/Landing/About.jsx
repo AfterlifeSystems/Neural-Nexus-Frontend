97382c680ca14651abd66c787e7e7eddb8b9b398
Evan Woods <22337004+efwoods@users.noreply.github.com>
2026-04-16 21:15:59 -0400

    updated frontend about

--- src/components/Landing/About.jsx
+++ src/components/Landing/About.jsx
@@ -15,8 +15,9 @@ export default function About() {
           Our mission is to empower human communication through advanced AI and
           neural technologies, creating a future where interactions are seamless
           and meaningful. We aim towards extending human consciousness,
-          improving health, and well being through self-awareness and augmenting
-          human capability and means of interfacing with technologies in future.
+          improving health, and well being through self-awareness, and
+          augmenting human capability and means of interfacing with technologies
+          in future.
         </p>
       </div>
     </section>
